90f4e3bd6c34313ca8ab456f487125725a698777
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-31 18:42:09 -0500

    Create Refuel.jsx

---
+++ core/components/Refuel.jsx
@@ -0,0 +1,14 @@
+export default function Refuel() {
+  return (
+    <div style={{
+      fontSize: '5em',
+      color: '#00aaff',
+      opacity: 0.3,
+      textAlign: 'center',
+      margin: '2rem 0',
+      userSelect: 'none'
+    }}>
+      ➰ Always full
+    </div>
+  );
+}
